b9f0256669392256d2827449752897915665b327
Alexis <lxs.bnds@gmail.com>
2020-01-23 13:39:19 -0800

    server connected

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS politics_db;
+
+CREATE DATABASE politics_db;
+
+USE politics_db;
--- server.js
+++ server.js
@@ -12,5 +12,11 @@ var handlebars = require("express-handlebars");
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
+var db = require("./models");
 ///routes variable and sequelize???
+
+db.sequelize.sync().then(function(){
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+      });
+});
