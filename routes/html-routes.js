9a2f27da4f3af5a42d6c91275372181aa9af67de
Alexis <lxs.bnds@gmail.com>
2020-01-25 15:42:14 -0800

    all navs flushed out html routes created, not throwing any information from the dbs

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,16 +8,20 @@ module.exports = function(app) {
     app.get("/profile", function(req,res){
         res.render("user");
     })
-    app.get("/searchHouse", function(req,res){
-        res.render("searchHouse");
-    })
-    app.get("/searchSenate", function(req,res){
-        res.render("searchSenate");
-    })
-    app.get("/searchCongress", function(req,res){
-        res.render("searchCongress");
-    })
 
+    app.get("/search/:congress", function(req,res){
+        let search =req.params.congress;
+        console.log(search);
+        if (search ==="senate"){
+            res.render("searchSenate");
+        }
+        else if (search ==="house"){
+            res.render("searchHouse");
+        }
+        else if (search ==="congress"){
+            res.render("searchCongress");
+        }
+    })
 
     // do we wanna do /search/:id with parameters
  //app.get app.post app.delete
