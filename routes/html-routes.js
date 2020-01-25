be55f6b457973692fe2a42b840acf0e9ccde6774
Alexis <lxs.bnds@gmail.com>
2020-01-25 14:11:36 -0800

    search pages created, need to talk to team about further improvements

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,5 +4,18 @@ module.exports = function(app) {
     app.get("/", function(req,res){
         res.render("index");
     })
+
+    app.get("/profile", function(req,res){
+        res.render("user");
+    })
+    app.get("/searchHouse", function(req,res){
+        res.render("searchHouse");
+    })
+    app.get("/searchSenate", function(req,res){
+        res.render("searchSenate");
+    })
+    app.get("/searchCongress", function(req,res){
+        res.render("searchCongress");
+    })
  //app.get app.post app.delete
   };
