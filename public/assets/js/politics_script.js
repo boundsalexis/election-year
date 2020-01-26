cdf6b03cf7c5d9a1932c45b1200170f37e0b5377
Alexis <lxs.bnds@gmail.com>
2020-01-26 14:16:04 -0800

    showing ian my work

--- public/assets/js/politics_script.js
+++ public/assets/js/politics_script.js
@@ -1,7 +1,19 @@
 $(function(){
-    // so everything is loaded before our js goes
+//make sure everything is loaded before we start
 
-    $("#newUser").on("submit", function(event){
+/////////////// *********index***********/////////////////
+    $("#signIn").on("submit", function(event){
+        event.preventDefault();
+        var signIn={
+            email: $("#signInEmail").val().trim(),
+            password: $("#signInPassword").val().trim()
+        }
+        console.log(signIn);
+    //add post route to check login against our db 
+    //add get route to route to profile page populated with
+    //saved table and comments?   
+    })
+    $("#newUser").on("click", function(event){
         event.preventDefault();
         var newUserData ={
             name: $("#username").val().trim(),
@@ -12,12 +24,30 @@ $(function(){
             password: $("#password").val().trim()
 
         }
-
-    // add post route here NEED ROUTE TO CONTINUE
+        console.log($("#location").val().trim())
+        console.log(newUserData);
+    //add post route to add this to  db
+    // add get route to route to profile page?
     })
 
+
+    //////
+    // decide if we want forgot password functionality
+    //////
+
+
+
+///////////////****** Search pages*********////////////
+
+   //
     $("#houseSearch").on("submit", function(event){
         event.preventDefault();
-        
+        var a = $("byParty").val();
+        var b = $("byState").val();
+        var c = $("byGender").val();
+        var d = $("byname").val();
+        console.log(a,b,c,d);
+        // set up conditionals for if one field selected 2 if 3 
     })
+ 
 })
