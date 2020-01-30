d27c71dcae5b8a62e26e0822ec984b6a2e982a34
Ian Toy <iet@ela.com>
2020-01-30 15:37:23 -0800

    removed unnecessary middleware per prev commit

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.set("view engine", "handlebars");
 // innerware for cors policy issue
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
+  // res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  // res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 // end innerware
