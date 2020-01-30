1e2e6a84f8f390fc4d997b1e496a6bd81987486f
Ian Toy <iet@ela.com>
2020-01-29 20:52:18 -0800

    completed pie chart display for representative pages on load. WooHoo!

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -11,26 +11,26 @@ $(document).ready(function () {
             type: "GET"
         }).then(function (response) {
 
-            console.log(response);
+            // console.log(response);
             let state = response.location;
             state = "CA";
             $.get("/api/senatorByState/" + state).then(function (data) {
-                console.log(data);
+                // console.log(data);
                 newRow=$("<tr>");
                 newRow.append("<th>Name</th>");
-                console.log(newRow)
+                // console.log(newRow)
                 $("#senators").append(newRow);
                 for (let i = 0; i < data.length; i++) {
                     dataRow =$("<tr>");
                     dataRow.append("<td>"+data[i].name+"</td>");
             
                     $("#senators").append(dataRow);
-                    console.log(dataRow);
+                    // console.log(dataRow);
 
                 }
             });
             $.get("/api/representativeByState/" + state).then(function (data) {
-                console.log(data);
+                // console.log(data);
                 newRow=$("<tr>");
                 newRow.append("<th>Name</th>");
                 newRow.append("<th>District</>");
